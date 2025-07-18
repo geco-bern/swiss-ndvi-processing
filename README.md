76654823b9dbebd9544d234c8cf73bd1bb5770a2
Samantha Biegel <samantha.r.biegel@gmail.com>
2025-07-18 16:36:38 +0200

    Add setup

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ conda create -n ndvi python=3.12
 conda activate ndvi
 conda install -c conda-forge gdal sqlite numpy zarr rasterio tqdm dask pandas taudem
 pip install pystac_client whitebox
+pip install -e .
 ```
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import find_namespace_packages, setup
+
+setup(
+    name="ndvi",
+    version="0.0.1",
+    url="https://github.com/samanthabiegel/swiss-ndvi-processing",
+    install_requires=[""],
+    packages=find_namespace_packages(),
+)
