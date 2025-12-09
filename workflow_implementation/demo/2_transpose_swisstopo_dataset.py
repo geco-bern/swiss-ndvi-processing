05ddf5dd42e3133a21d98f24cde5eba405b85ca7
FrancescoGrossi-unimi <francesco.grossi@studenti.unimi.it>
2025-12-09 15:23:25 +0100

    update demo

--- workflow_implementation/demo/2_transpose_swisstopo_dataset.py
+++ workflow_implementation/demo/2_transpose_swisstopo_dataset.py
@@ -1,15 +1,15 @@
 """
 Transpose Swisstopo NDVI/NDSI dataset from (T, N) to (N, T) layout using Dask.
 """
-# nohup python -u /home/francesco/data_scratch/swiss-ndvi-processing/workflow_implementation/2_transpose_swisstopo_dataset.py >  /home/francesco/data_scratch/swiss-ndvi-processing/workflow_implementation/transpose_swisstopo.log &
+# nohup python -u /home/francesco/data_scratch/swiss-ndvi-processing/workflow_implementation/demo/2_transpose_swisstopo_dataset.py > /home/francesco/data_scratch/swiss-ndvi-processing/workflow_implementation/transpose_swisstopo.log &
 
 import dask.array as da
 from dask.distributed import Client, LocalCluster
 import shutil
 import os
 
-SOURCE_ZARR = "/data_3/scratch/francesco/processed/ndvi_dataset_spatial.zarr"
-TRANSPOSED_ZARR = "/data_3/scratch/francesco/processed/ndvi_dataset_temporal.zarr"
+SOURCE_ZARR = "/data_3/scratch/fabian/data-swiss-ndvi-processing_redownload_Apr2017-Nov2025/ndvi_dataset_spatial_Apr2017-Nov2025.zarr"
+TRANSPOSED_ZARR = "/data_3/scratch/francesco/processed/all_ndvi_dataset_temporal.zarr"
 DASK_LOCAL_DIRECTORY = "/data_3/francesco/dask_worker_space"
 
 if os.path.exists(TRANSPOSED_ZARR):
@@ -25,6 +25,8 @@ def transpose_zarr(source_zarr, target_zarr, component="ndvi"):
         local_directory=DASK_LOCAL_DIRECTORY,
     )
     client = Client(cluster)
+
+    print(client.dashboard_link)
 
     src = da.from_zarr(source_zarr, component=component)
     T, N = src.shape
@@ -46,9 +48,6 @@ if __name__ == "__main__":
     print("done ndvi")
     transpose_zarr(SOURCE_ZARR, TRANSPOSED_ZARR, component="ndsi")
     print("done ndsi")
-
-    if os.path.exists(SOURCE_ZARR):
-        shutil.rmtree(SOURCE_ZARR)
 
     shutil.rmtree(DASK_LOCAL_DIRECTORY)
 
