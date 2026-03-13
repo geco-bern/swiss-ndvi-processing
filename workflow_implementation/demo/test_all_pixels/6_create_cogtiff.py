56ba294028cf5c6cbe32dfcad0b90bc0a823f625
fabern <10245680+fabern@users.noreply.github.com>
2026-03-13 00:41:50 +0000

    Simplify cogtiff creation
    Previous cogtiff were only 60x60. This rewrites the cogtiff creation. Greatly simplifying the code.

--- workflow_implementation/demo/test_all_pixels/6_create_cogtiff.py
+++ workflow_implementation/demo/test_all_pixels/6_create_cogtiff.py
@@ -1,6 +1,7 @@
 # nohup python -u /home/Shared/UniBe-swiss-ndvi/GitHub/swiss-ndvi-processing/workflow_implementation/demo/test_all_pixels/6_create_cogtiff.py> /home/Shared/UniBe-swiss-ndvi/GitHub/swiss-ndvi-processing/workflow_implementation/demo/test_all_pixels/tif.log 2>&1 &
 
 import rasterio
+import rioxarray
 import numpy as np
 import xarray as xr
 import dask.array as da
@@ -9,7 +10,6 @@ import pandas as pd
 import pystac_client
 import os
 import argparse
-
 
 # nohup python -u /home/Shared/UniBe-swiss-ndvi/GitHub/swiss-ndvi-processing/workflow_implementation/demo/test_all_pixels/6_create_cogtiff.py > /home/Shared/UniBe-swiss-ndvi/GitHub/swiss-ndvi-processing/workflow_implementation/demo/test_all_pixels/6_tiff.log 2>&1 &
 def get_swisstopo_sentinel_dates(start, end): # start='2025-12-01', end='2026-02-16' # TODO: we already have a similar function => consolidate
@@ -49,10 +49,11 @@ def get_swisstopo_sentinel_dates(start, end): # start='2025-12-01', end='2026-02
 #INPUT_BASE = "/mnt/data1/UniBe-swiss-ndvi/data/demo_all_pixel/05_processed/"
 INPUT_BASE = "/mnt/data1/UniBe-swiss-ndvi/tmp_ndvi_05_processed.zarr"
 OUTPUT_TIFF_BASE = "/mnt/data1/UniBe-swiss-ndvi/data/tiffs/"
-# forest_mask = "../../data/input/forest_mask.npy"
-
 
 os.makedirs(OUTPUT_TIFF_BASE, exist_ok=True)
+
+# ssh dash-WS02-GECO; rsync -avh --progress -e 'ssh -p 2222' /data_2/scratch/sbiegel/processed/forest_mask.npy fabian-bernhard@dac3.ddns.net:/mnt/data1/UniBe-swiss-ndvi/data/forest_mask.npy
+# FOREST_MASK = "/mnt/data1/UniBe-swiss-ndvi/data/forest_mask.npy" # NOTE: this is different from 'mask_array'  # TODO: FOREST_MASK is unused
 
 
 parser = argparse.ArgumentParser()
@@ -87,152 +88,112 @@ dates_tiff_array = np.arange(
     end_tiff_date + np.timedelta64(1, 'D'), 
     dtype='datetime64[D]')
 
-store2 = xarray.open_zarr(INPUT_BASE)
 
-dates_done = [s[:8] for s in os.listdir(OUTPUT_TIFF_BASE)]
 
-#curr_date = dates_tiff_array[0]
-#for curr_date in dates_tiff_array:
-curr_date_str = pd.to_datetime(curr_date).strftime('%Y%m%d')
-if (curr_date_str in dates_done):
-    print(f"Skipping file (already exported): {curr_date_str}.tiff")
-    break
 
-# Subst single date slice
-ndvi_array = store2.sel(date=curr_date)['ndvi_processed']
-mask_array = store2.sel(date=curr_date)['mask_array']
-    # mask_array == 0: the data is not an observation and is yet to be smoothed
-    # mask_array == 1: the data is not an observation and is smoothed
-    # mask_array == 2: the data is an observation and is yet to be smoothed
-    # mask_array == 3: the data is an observation and is smoothed
-    # mask_array == 4: the data is an observation and is an outlier
 
-x_array = store2['x']
-y_array = store2['y']
+# Load (processed) NDVI data set to output specific dates
+NDVI_processed = xr.open_zarr(INPUT_BASE)
 
-# NOTE: previously we had something like: 
-#   if (np.sum((mask_array.data.compute() == 3)|(mask_array.data.compute() == 1))/ len(mask_array.data)) > 0.9:
+# Define grid underlying PixelID and needed transformations
+# TODO: actually append this similarly to x and y as (x_idx, and y_idx) to historic data set already.
+#       this would simplify handling of historic data set
 
-ds_tiff = xr.Dataset({
-    'ndvi': (['pixel'], ndvi_array.data),
-    'mask': (['pixel'], mask_array.data),
-    'x': (['pixel'], x_array.data),
-    'y': (['pixel'], y_array.data)
-})
-
-# # Metadata
-# ds_tiff.attrs = {
-#     'crs': 'EPSG:2056',
-#     #'date_value': date, # TODO: this should be added again (as string)
-#     'total_pixels': len(ds_tiff.pixel)
-# }
-
-# # dtypes
-# encoding = {
-#     'ndvi': {'zlib': True, 'complevel': 5, 'dtype': np.int16},
-#     'mask': {'zlib': True, 'complevel': 5, 'dtype': np.int8},
-#     'x': {'zlib': True, 'dtype': np.int32},
-#     'y': {'zlib': True, 'dtype': np.int32}
-# }
-
-output_tiff = f"{OUTPUT_TIFF_BASE}{pd.to_datetime(curr_date).strftime('%Y%m%d')}.tiff"
-
-# ds_tiff.to_netcdf(output_tiff, encoding=encoding) # Is this Tiff format? This looks more like netcdf. Where did they give specifications?
-
-# Raster info
+# Raster info (of downloaded product)
 height, width = 24542, 37728
 left, bottom = 2474090.0, 1065110.0
 px = 10.0
 top = bottom + height * px
+# NOTE: 24542*37728 # = 925 Mio > 106 Mio pixel in ndvi_hist. This seems correct.
 
-# ----- center cooridnates  -----
-center_x, center_y = 2694491.82, 1126023.20
-# Rectangle corners (UL and BR)
-UL_x, UL_y = center_x - 300, center_y - 300 
-BR_x, BR_y = center_x + 300, center_y + 300
+# Define transform between row,col to coord (upper-left origin, pixel sizes)
+trans = rasterio.transform.from_origin(left, top, px, px)
 
+# Test how to use this transformation definition:
+# rasterio.transform.xy(trans, [0], [0])    # returns center coordinates of first upper left pixel
+# rasterio.transform.xy(trans, [10], [10])  # returns center coordinates of tenth upper left pixel (i.e. is more east and more south)
+# rasterio.transform.xy(trans, [0, 10], [0, 10]) # this goes from pixel index to coordinate
 
-# ----- compute pixel window (row 0 = top) -----
-x_min, x_max = min(UL_x, BR_x), max(UL_x, BR_x)
-y_min, y_max = min(UL_y, BR_y), max(UL_y, BR_y)
+# rows, cols = np.nonzero(forest_mask) # forest_mask = np.load(FOREST_MASK) # TODO: FOREST_MASK is unused
+# rows, cols = np.nonzero([[1, 0, 1],[0, 0, 0],[0, 0, 0]]) # returns [0 0] and [0 2]
+# ids = np.arange(len(rows))
+# xs, ys = rasterio.transform.xy(trans, rows, cols) # returns:  (array([2474095., 2474115.]), array([1310525., 1310525.]))
+# rasterio.transform.rowcol(trans, xs, ys)          # recovers: (array([0, 0]), array([0, 2]))
 
-col_min = int(np.floor((x_min - left) / px))
-col_max = int(np.floor((x_max - left) / px))
+# coord_lookup = pd.DataFrame({
+#     'pixelID': ids,
+#     'x': xs,
+#     'y': ys
+# })
 
-row_min = int(np.floor((top - y_max) / px))
-row_max = int(np.floor((top - y_min) / px))
-
-# clip to bounds
-col_min = max(0, min(width - 1, col_min))
-col_max = max(0, min(width - 1, col_max))
-row_min = max(0, min(height - 1, row_min))
-row_max = max(0, min(height - 1, row_max))
-
-win_cols = col_max - col_min + 1
-win_rows = row_max - row_min + 1
-
-# ----- load mask -----
-forest_mask = "../../data/input/forest_mask.npy" # TODO: note this is not mask_array
-mask = np.load(forest_mask)
-assert mask.shape == (height, width), f"Mask shape {mask.shape} != raster {(height, width)}"
-mask_array.shape
-#mask.size
-mask_flat = mask.ravel(order="C")
-masked_positions = np.flatnonzero(mask_flat)
-n_masked = masked_positions.size
-
-# build index map from full array -> masked array
-idx_map = np.full(mask_flat.shape[0], -1, dtype=np.int64)
-idx_map[masked_positions] = np.arange(n_masked, dtype=np.int64)
-
-# ----- compute flat indices in window -----
-rows = np.arange(row_min, row_max + 1, dtype=np.int64)
-cols = np.arange(col_min, col_max + 1, dtype=np.int64)
-rr, cc = np.meshgrid(rows, cols, indexing="ij")
-full_flat_idx = (rr * width + cc).ravel()
-
-masked_idx_in_window = idx_map[full_flat_idx]
-is_masked = masked_idx_in_window >= 0
-n_masked_in_window = is_masked.sum()
-
-sel = masked_idx_in_window[is_masked].tolist()
-
-values = np.empty(n_masked_in_window, dtype=float)
-window = np.full(win_rows * win_cols, np.nan, dtype=float)
-
-window[is_masked] = 1 # TODO switch back to: ndvi_array.values # TODO: make this work (with subset)
-window = window.reshape((win_rows, win_cols))
-
-arr = np.nan_to_num(window, nan=-9999).astype('int16')
-
-x_min = 2694491 - 300
-y_max = 1126023 - 300
-pixel_width = 10
-pixel_height = 10
-
-# TODO: activate tiff output: transform = from_origin(x_min, y_max, pixel_width, pixel_height) # TODO make this work
-# TODO: activate tiff output: 
-# TODO: activate tiff output: # TODO: define output_tiff = ... from 
-# TODO: activate tiff output: with rasterio.open(
-# TODO: activate tiff output:     output_tiff,
-# TODO: activate tiff output:     'w',
-# TODO: activate tiff output:     driver='COG',
-# TODO: activate tiff output:     height=arr.shape[0],
-# TODO: activate tiff output:     width=arr.shape[1],
-# TODO: activate tiff output:     count=1,
-# TODO: activate tiff output:     dtype=arr.dtype,
-# TODO: activate tiff output:     crs='EPSG:2056', 
-# TODO: activate tiff output:     # transform=transform, # TODO: make this work
-# TODO: activate tiff output:     nodata=np.nan,
-# TODO: activate tiff output:     compress='deflate',
-# TODO: activate tiff output:     tiled=True
-# TODO: activate tiff output: ) as dst:
-# TODO: activate tiff output:     dst.write(arr, 1) 
-
-#TODO: potentially also store mask_array as separate TIFF
-
-print(f"Created {output_tiff}")
+# coords = list(zip(xs, ys))
+# plt.plot(xs, ys)
+# plt.plot(coords)
 
 
 
 
+
+# Loop over dates
+dates_done = [s[:8] for s in os.listdir(OUTPUT_TIFF_BASE)]
+
+for curr_date in dates_tiff_array:
+    curr_date_str = pd.to_datetime(curr_date).strftime('%Y%m%d')
+    if (curr_date_str in dates_done):
+        print(f"Skipping file (already exported): {curr_date_str}.tiff")
+    else:
+        # Initialize regular grid filled with NaN for tiff to be filled with values
+        rows, cols = rasterio.transform.rowcol(
+            trans, 
+            NDVI_processed.x.values, 
+            NDVI_processed.y.values)
+
+        height = rows.max() + 1
+        width = cols.max() + 1
+        grid_ndvi = np.full((height, width), np.nan) # TODO: add again: , dtype=np.int16
+        grid_mask = np.full((height, width), np.nan) # TODO: add again: , dtype=np.int16
+
+        # Fill regular grid with values
+        grid_ndvi[rows, cols] = NDVI_processed.sel(date=curr_date)['ndvi_processed'].values
+        grid_mask[rows, cols] = NDVI_processed.sel(date=curr_date)['mask_array'].values
+
+        # Transform back into a xarray/rioxarray DataArray that spans a regular x-y-grid
+        NDVI_processed_curr_date_gridded = xr.DataArray(grid_ndvi,dims=("y", "x"))
+        NDVI_processed_curr_date_gridded = NDVI_processed_curr_date_gridded.rio.write_transform(trans)
+        NDVI_processed_curr_date_gridded = NDVI_processed_curr_date_gridded.rio.write_crs("EPSG:2056")
+
+        NDVI_status_curr_date_gridded = xr.DataArray(grid_mask,dims=("y", "x"))
+        NDVI_status_curr_date_gridded = NDVI_status_curr_date_gridded.rio.write_transform(trans)
+        NDVI_status_curr_date_gridded = NDVI_status_curr_date_gridded.rio.write_crs("EPSG:2056")
+
+        # Output as cloud optimized Geotiff:
+        output_tiff_ndvi = f"{OUTPUT_TIFF_BASE}{pd.to_datetime(curr_date).strftime('%Y%m%d')}.tiff"
+        output_tiff_mask = f"{OUTPUT_TIFF_BASE}{pd.to_datetime(curr_date).strftime('%Y%m%d')}_mask.tiff"
+
+        NDVI_processed_curr_date_gridded.rio.to_raster(
+            output_tiff_ndvi,
+            driver="COG",
+            compress="deflate",
+            dtype="int16",
+        )
+        NDVI_status_curr_date_gridded.rio.to_raster(
+            output_tiff_mask,
+            driver="COG",
+            compress="deflate",
+            dtype="int16",
+        )
+        # mask_array == 0: the data is not an observation and is yet to be smoothed
+        # mask_array == 1: the data is not an observation and is smoothed
+        # mask_array == 2: the data is an observation and is yet to be smoothed
+        # mask_array == 3: the data is an observation and is smoothed
+        # mask_array == 4: the data is an observation and is an outlier
+
+        print(f"Created {output_tiff_ndvi}")
+        print(f"Created {output_tiff_mask}")
+
+
+# rsync 
+# rsync -avh --progress -e 'ssh -p 2222' fabian-bernhard@dac3.ddns.net:/mnt/data1/UniBe-swiss-ndvi/data/tiffs/20250829.tiff ~/Downloads/test/
+# rsync -avh --progress -e 'ssh -p 2222' fabian-bernhard@dac3.ddns.net:/mnt/data1/UniBe-swiss-ndvi/data/tiffs/20250829.tiff ~/Downloads/test/
+# gdal_translate -of PNG -scale Downloads/test/20250829.tiff Downloads/test/20250829.tiff.png
+# open Downloads/test/20250829.tiff.png
